193d44e9a068ce184ac2d87bc96f6840f4f267ad
张博 <zhangbo3@yy.com>
2014-10-09 17:32:07 +0800

    Implement a raw http proxy server

--- proxy/main.go
+++ proxy/main.go
@@ -4,31 +4,58 @@ import (
 	"fmt"
 	"github.com/Mr8/goproxy/utils"
 	"net"
-	"strings"
 )
+
+var CLRF = []byte{0x0d, 0x0a, 0x0d, 0x0d}
+
+func RemoteCon(hp *utils.HttpParser) (net.Conn, error) {
+	return net.Dial("tcp", fmt.Sprintf("%s:%s", hp.Host, hp.Port))
+}
 
 func HandlerCon(c net.Conn) {
 	defer c.Close()
+	var (
+		header *utils.HttpParser
+		msg    []byte
+	)
+
 	C := NewCon(c)
+
+	sz, err := C.Read()
+	if sz == 0 || err != nil {
+		return
+	}
+	msg = C.GetMsg()
+	header, err = utils.Parser(msg)
+	fmt.Printf("Parsed HTTP Header--> \n%s\n", header)
+	fmt.Printf("Received HTTP Header--> \n%s\n", string(msg))
+	tBuf, err := TransferHTTP(C.GetMsg(), header)
+	fmt.Printf("TransferHTTP--> \n%s\n", string(tBuf))
+
+	if header == nil || msg == nil {
+		return
+	}
+
+	fmt.Printf(fmt.Sprintf("Make connection to remote %s:%s", header.Host, header.Port))
+	conn, err := RemoteCon(header)
+	if err != nil {
+		return
+	}
+	conn.Write(msg)
+
 	for {
-		sz, err := C.Read()
+		buf := make([]byte, 10240)
+		sz, err := conn.Read(buf)
 		if sz == 0 || err != nil {
 			return
 		}
-		msg := C.GetMsg()
-		header, err := utils.Parser(msg)
-		if err != nil {
-			C.Write(C.GetMsg())
-			return
-		}
-		fmt.Printf("Parsed HTTP Header %s", header)
-		C.Write(C.GetMsg())
+		C.Write(buf)
 	}
 }
 
 func main() {
 	fmt.Printf("Start proxy server\n")
-	l, err := net.Listen("tcp", "127.0.0.1:1121")
+	l, err := net.Listen("tcp4", "0.0.0.0:1121")
 	if err != nil {
 		fmt.Printf("Listen to port error :%s\n", err.Error())
 	}
